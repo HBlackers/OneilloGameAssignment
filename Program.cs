2f1e10c3e207685b89f98ad7b3595dd6c2560424
Harrison Blackburn <hblackburn@redev.co.uk>
2023-10-19 09:22:18 +0100

    application runs correct form

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using O_NeilloGame;
+
 namespace O_neilloGame
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace O_neilloGame
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
