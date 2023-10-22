036df8130c60fb101c627841c7f102f98b7c8d56
Harrison Blackburn <hblackburn@redev.co.uk>
2023-10-23 00:43:34 +0100

    Main interface changes

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -29,97 +29,117 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
-            this.Menu = new System.Windows.Forms.ToolStrip();
-            this.btnGame = new System.Windows.Forms.ToolStripButton();
-            this.btnSettings = new System.Windows.Forms.ToolStripButton();
-            this.btnHelp = new System.Windows.Forms.ToolStripButton();
-            this.pnl_Game = new System.Windows.Forms.Panel();
-            this.flp_GameInfo = new System.Windows.Forms.FlowLayoutPanel();
-            this.Menu.SuspendLayout();
-            this.SuspendLayout();
+            Menu = new ToolStrip();
+            btnGame = new ToolStripButton();
+            btnSettings = new ToolStripButton();
+            btnHelp = new ToolStripButton();
+            flp_GameInfo = new FlowLayoutPanel();
+            tlpGameBoard = new TableLayoutPanel();
+            Menu.SuspendLayout();
+            SuspendLayout();
             // 
             // Menu
             // 
-            this.Menu.BackColor = System.Drawing.Color.Chocolate;
-            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.btnGame,
-            this.btnSettings,
-            this.btnHelp});
-            this.Menu.Location = new System.Drawing.Point(0, 0);
-            this.Menu.Name = "Menu";
-            this.Menu.Size = new System.Drawing.Size(844, 25);
-            this.Menu.TabIndex = 0;
-            this.Menu.Text = "toolStrip1";
+            Menu.BackColor = Color.Chocolate;
+            Menu.ImageScalingSize = new Size(20, 20);
+            Menu.Items.AddRange(new ToolStripItem[] { btnGame, btnSettings, btnHelp });
+            Menu.Location = new Point(0, 0);
+            Menu.Name = "Menu";
+            Menu.Size = new Size(982, 27);
+            Menu.TabIndex = 0;
+            Menu.Text = "toolStrip1";
             // 
             // btnGame
             // 
-            this.btnGame.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnGame.Image = ((System.Drawing.Image)(resources.GetObject("btnGame.Image")));
-            this.btnGame.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnGame.Name = "btnGame";
-            this.btnGame.Size = new System.Drawing.Size(42, 22);
-            this.btnGame.Text = "Game";
+            btnGame.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnGame.Image = (Image)resources.GetObject("btnGame.Image");
+            btnGame.ImageTransparentColor = Color.Magenta;
+            btnGame.Name = "btnGame";
+            btnGame.Size = new Size(52, 24);
+            btnGame.Text = "Game";
             // 
             // btnSettings
             // 
-            this.btnSettings.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnSettings.Image = ((System.Drawing.Image)(resources.GetObject("btnSettings.Image")));
-            this.btnSettings.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnSettings.Name = "btnSettings";
-            this.btnSettings.Size = new System.Drawing.Size(53, 22);
-            this.btnSettings.Text = "Settings";
+            btnSettings.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnSettings.Image = (Image)resources.GetObject("btnSettings.Image");
+            btnSettings.ImageTransparentColor = Color.Magenta;
+            btnSettings.Name = "btnSettings";
+            btnSettings.Size = new Size(66, 24);
+            btnSettings.Text = "Settings";
             // 
             // btnHelp
             // 
-            this.btnHelp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnHelp.Image = ((System.Drawing.Image)(resources.GetObject("btnHelp.Image")));
-            this.btnHelp.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnHelp.Name = "btnHelp";
-            this.btnHelp.Size = new System.Drawing.Size(36, 22);
-            this.btnHelp.Text = "Help";
-            // 
-            // pnl_Game
-            // 
-            this.pnl_Game.BackColor = System.Drawing.Color.White;
-            this.pnl_Game.Location = new System.Drawing.Point(12, 47);
-            this.pnl_Game.Name = "pnl_Game";
-            this.pnl_Game.Size = new System.Drawing.Size(776, 248);
-            this.pnl_Game.TabIndex = 1;
+            btnHelp.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnHelp.Image = (Image)resources.GetObject("btnHelp.Image");
+            btnHelp.ImageTransparentColor = Color.Magenta;
+            btnHelp.Name = "btnHelp";
+            btnHelp.Size = new Size(45, 24);
+            btnHelp.Text = "Help";
             // 
             // flp_GameInfo
             // 
-            this.flp_GameInfo.BackColor = System.Drawing.Color.Chocolate;
-            this.flp_GameInfo.Location = new System.Drawing.Point(12, 526);
-            this.flp_GameInfo.Name = "flp_GameInfo";
-            this.flp_GameInfo.Size = new System.Drawing.Size(446, 130);
-            this.flp_GameInfo.TabIndex = 2;
+            flp_GameInfo.BackColor = Color.Chocolate;
+            flp_GameInfo.Location = new Point(191, 712);
+            flp_GameInfo.Margin = new Padding(4, 5, 4, 5);
+            flp_GameInfo.Name = "flp_GameInfo";
+            flp_GameInfo.Size = new Size(600, 200);
+            flp_GameInfo.TabIndex = 2;
+            // 
+            // tlpGameBoard
+            // 
+            tlpGameBoard.BackColor = Color.Black;
+            tlpGameBoard.ColumnCount = 2;
+            tlpGameBoard.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.RowStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.RowStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.RowStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.RowStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.RowStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.RowStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.RowStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.RowStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tlpGameBoard.Location = new Point(42, 54);
+            tlpGameBoard.Name = "tlpGameBoard";
+            tlpGameBoard.RowCount = 2;
+            tlpGameBoard.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tlpGameBoard.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tlpGameBoard.Size = new Size(900, 600);
+            tlpGameBoard.TabIndex = 3;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(844, 821);
-            this.Controls.Add(this.flp_GameInfo);
-            this.Controls.Add(this.pnl_Game);
-            this.Controls.Add(this.Menu);
-            this.Name = "Main";
-            this.Text = "O\'Neillo Game";
-            this.Menu.ResumeLayout(false);
-            this.Menu.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(982, 953);
+            Controls.Add(tlpGameBoard);
+            Controls.Add(flp_GameInfo);
+            Controls.Add(Menu);
+            ForeColor = SystemColors.ControlLightLight;
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Main";
+            Text = "O'Neillo Game";
+            Menu.ResumeLayout(false);
+            Menu.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private System.Windows.Forms.ToolStrip Menu;
-        private System.Windows.Forms.ToolStripButton btnGame;
-        private System.Windows.Forms.ToolStripButton btnSettings;
-        private System.Windows.Forms.ToolStripButton btnHelp;
-        private System.Windows.Forms.Panel pnl_Game;
-        private System.Windows.Forms.FlowLayoutPanel flp_GameInfo;
+        private ToolStrip Menu;
+        private ToolStripButton btnGame;
+        private ToolStripButton btnSettings;
+        private ToolStripButton btnHelp;
+        private FlowLayoutPanel flp_GameInfo;
+        private TableLayoutPanel tlpGameBoard;
     }
 }
 
